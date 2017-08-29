b725c6df725ae5452bee092f3fd3fc200935dd7f
kai-xie <qikai_xie@163.com>
2017-08-29 11:01:45 +0800

    rename compress layers(.cpp, .hpp); revised cinner_product_layer.cpp.

--- src/caffe/layers/cinner_product_layer.cpp
+++ src/caffe/layers/cinner_product_layer.cpp
@@ -1,13 +1,8 @@
 #include <vector>
 
-#include "caffe/blob.hpp"
-#include "caffe/common.hpp"
 #include "caffe/filler.hpp"
-#include "caffe/layer.hpp"
-#include "caffe/util/io.hpp"
+#include "caffe/layers/cinner_product_layer.hpp"
 #include "caffe/util/math_functions.hpp"
-#include "caffe/vision_layers.hpp"
-#include <cmath>
 
 namespace caffe {
 
@@ -16,6 +11,7 @@ void CInnerProductLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   const int num_output = this->layer_param_.inner_product_param().num_output();
   bias_term_ = this->layer_param_.inner_product_param().bias_term();
+  transpose_ = this->layer_param_.inner_product_param().transpose();
   N_ = num_output;
   const int axis = bottom[0]->CanonicalAxisIndex(
       this->layer_param_.inner_product_param().axis());
@@ -27,22 +23,27 @@ void CInnerProductLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
   if (this->blobs_.size() > 0) {
     LOG(INFO) << "Skipping parameter initialization";
   } else {
-    if (this->bias_term_) {
+    if (bias_term_) {
       this->blobs_.resize(2);
     } else {
       this->blobs_.resize(1);
     }
-    // Intialize the weight
+    // Initialize the weights
     vector<int> weight_shape(2);
-    weight_shape[0] = N_;
-    weight_shape[1] = K_;
+    if (transpose_) {
+      weight_shape[0] = K_;
+      weight_shape[1] = N_;
+    } else {
+      weight_shape[0] = N_;
+      weight_shape[1] = K_;
+    }
     this->blobs_[0].reset(new Blob<Dtype>(weight_shape));
     // fill the weights
     shared_ptr<Filler<Dtype> > weight_filler(GetFiller<Dtype>(
         this->layer_param_.inner_product_param().weight_filler()));
     weight_filler->Fill(this->blobs_[0].get());
     // If necessary, intiialize and fill the bias term
-    if (this->bias_term_) {
+    if (bias_term_) {
       vector<int> bias_shape(1, N_);
       this->blobs_[1].reset(new Blob<Dtype>(bias_shape));
       shared_ptr<Filler<Dtype> > bias_filler(GetFiller<Dtype>(
@@ -53,7 +54,8 @@ void CInnerProductLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
   this->param_propagate_down_.resize(this->blobs_.size(), true);
   
   /************ For dynamic network surgery ***************/
-	CInnerProductParameter cinner_param = this->layer_param_.cinner_product_param();
+  CInnerProductParameter cinner_param = 
+    this->layer_param_.cinner_product_param();
 	
   if(this->blobs_.size()==2 && (this->bias_term_)){
     this->blobs_.resize(4);
@@ -119,9 +121,10 @@ template <typename Dtype>
 void CInnerProductLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
   
-  const Dtype* weight = this->blobs_[0]->mutable_cpu_data();    
+  const Dtype* weight = this->blobs_[0]->cpu_data();    
   Dtype* weightMask = this->blobs_[2]->mutable_cpu_data(); 
   Dtype* weightTmp = this->weight_tmp_.mutable_cpu_data(); 
+  
   const Dtype* bias = NULL;
   Dtype* biasMask = NULL;  
   Dtype* biasTmp = NULL;
@@ -188,7 +191,7 @@ void CInnerProductLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
 		}
 	}    
 	
-  // Calculate the current (masked) weight and bias
+  // Calculate the acctually forwarded (masked) weight and bias
 	for (unsigned int k = 0;k < this->blobs_[0]->count(); ++k) {
 		weightTmp[k] = weight[k]*weightMask[k];
 	}
@@ -201,7 +204,8 @@ void CInnerProductLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
 	// Forward calculation with (masked) weight and bias 
   const Dtype* bottom_data = bottom[0]->cpu_data();
   Dtype* top_data = top[0]->mutable_cpu_data();
-  caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasTrans, M_, N_, K_, (Dtype)1.,
+  caffe_cpu_gemm<Dtype>(CblasNoTrans, transpose_ ? CblasNoTrans : CblasTrans, 
+      M_, N_, K_, (Dtype)1.,
       bottom_data, weightTmp, (Dtype)0., top_data);
   if (bias_term_) {
     caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, M_, N_, 1, (Dtype)1.,
@@ -220,11 +224,21 @@ void CInnerProductLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
 		Dtype* weight_diff = this->blobs_[0]->mutable_cpu_diff();		      
     const Dtype* bottom_data = bottom[0]->cpu_data();    
     // Gradient with respect to weight
+    // ?? check this loop
 		for (unsigned int k = 0;k < this->blobs_[0]->count(); ++k) {
 			weight_diff[k] = weight_diff[k]*weightMask[k];
-		}		
-    caffe_cpu_gemm<Dtype>(CblasTrans, CblasNoTrans, N_, K_, M_, (Dtype)1.,
-        top_diff, bottom_data, (Dtype)1., weight_diff);    
+    }		
+    if (transpose_) {
+      caffe_cpu_gemm<Dtype>(CblasTrans, CblasNoTrans,
+          K_, N_, M_,
+          (Dtype)1., bottom_data, top_diff,
+          (Dtype)1., weight_diff);
+    } else {
+      caffe_cpu_gemm<Dtype>(CblasTrans, CblasNoTrans,
+          N_, K_, M_,
+          (Dtype)1., top_diff, bottom_data,
+          (Dtype)1., weight_diff);
+    }
   }
   if (bias_term_ && this->param_propagate_down_[1]) {
 		const Dtype* biasMask = this->blobs_[3]->cpu_data();
@@ -239,9 +253,17 @@ void CInnerProductLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
   if (propagate_down[0]) {
     const	Dtype* weightTmp = this->weight_tmp_.cpu_data();
     // Gradient with respect to bottom data
-    caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, M_, K_, N_, (Dtype)1.,
-        top_diff, weightTmp, (Dtype)0.,
-        bottom[0]->mutable_cpu_diff());
+    if (transpose_) {
+      caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasTrans,
+          M_, K_, N_,
+          (Dtype)1., top_diff, this->blobs_[0]->cpu_data(),
+          (Dtype)0., bottom[0]->mutable_cpu_diff());
+    } else {
+      caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans,
+          M_, K_, N_,
+          (Dtype)1., top_diff, this->blobs_[0]->cpu_data(),
+          (Dtype)0., bottom[0]->mutable_cpu_diff());
+    }
   }
 }
 

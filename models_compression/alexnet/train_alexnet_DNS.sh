8bc4e5fd4951acf82c920763b65991a7c261a57f
kai-xie <qikai_xie@163.com>
2017-09-08 11:42:51 +0800

    add *.sh and *.prototxt files for alexnet training.
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch unstable
     Your branch is up-to-date with 'origin/unstable'.
    
     Changes to be committed:
    	modified:   examples/mnist/lenet_train_test_DNS.prototxt
    	new file:   models_compression/alexnet/alexnet_deploy.prototxt
    	new file:   models_compression/alexnet/alexnet_solver_DNS.prototxt
    	new file:   models_compression/alexnet/train_alexnet_DNS.sh
    	modified:   models_compression/alexnet/train_val_DNS.prototxt

---
+++ models_compression/alexnet/train_alexnet_DNS.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -e
+
+./build/tools/caffe train \
+    --solver=/models_compression/alexnet/alexnet_solver_DNS.prototxt \
+    --weights=/models/bvlc_alexnet/bvlc_alexnet.caffemodel $@ 
+    
